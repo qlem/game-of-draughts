d8b803a2b0e273bd8b846d0935843b42463446b4
qlem <clement.dommerc@gmail.com>
2018-11-14 23:04:19 +0000

    draw board

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget, QAction, QFileDialog, QWidget, QLabel, \
-    QPushButton, QColorDialog, QVBoxLayout, QGridLayout, QRadioButton, QButtonGroup, QSlider, QMessageBox
+    QPushButton, QColorDialog, QVBoxLayout, QGridLayout, QRadioButton, QButtonGroup, QSlider, QMessageBox, QFrame
 from PyQt5.QtGui import QIcon, QImage, QPainter, QPen, QColor
 import sys
 import os
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QRect, QRectF
 from enum import Enum
 
 
@@ -25,15 +25,6 @@ class Player1Widget(QWidget):
 
         self.setStyleSheet("background: red")
 
-        print("player 1")
-        print(self.width())
-        print(self.height())
-
-    def resizeEvent(self, event):
-        print("player 1")
-        print(self.width())
-        print(self.height())
-
 
 class Player2Widget(QWidget):
     def __init__(self):
@@ -47,44 +38,51 @@ class Player2Widget(QWidget):
 
         self.setStyleSheet("background: blue")
 
-        print("player 2")
-        print(self.width())
-        print(self.height())
 
-    def resizeEvent(self, event):
-        print("player 2")
-        print(self.width())
-        print(self.height())
-
-
-class GameBoardWidget(QWidget):
+class GameBoardWidget(QFrame):
     def __init__(self):
         super().__init__()
 
-        label = QLabel("game board")
+        self.BOARD_SIZE = 0
+        self.CELL_SIZE = 0
 
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        self.setLayout(layout)
+    def draw_board(self):
+        painter = QPainter(self)
 
-        self.setStyleSheet("background: green")
+        pen = QPen(Qt.black, 6, Qt.SolidLine, Qt.FlatCap, Qt.MiterJoin)
+        painter.setPen(pen)
+        painter.drawLine(0, 0, self.BOARD_SIZE, 0)
+        painter.drawLine(self.BOARD_SIZE, 0, self.BOARD_SIZE, self.BOARD_SIZE)
+        painter.drawLine(self.BOARD_SIZE, self.BOARD_SIZE, 0, self.BOARD_SIZE)
+        painter.drawLine(0, 0, 0, self.BOARD_SIZE)
 
-        print("game board")
-        print(self.width())
-        print(self.height())
+        for i in range(8):
+            for j in range(8):
+                x = j * self.CELL_SIZE
+                y = i * self.CELL_SIZE
+                if i % 2 == 0 and j % 2 > 0:
+                    painter.fillRect(x, y, self.CELL_SIZE, self.CELL_SIZE, Qt.black)
+                elif i % 2 > 0 and j % 2 == 0:
+                    painter.fillRect(x, y, self.CELL_SIZE, self.CELL_SIZE, Qt.black)
+
+    def paintEvent(self, event):
+        print("paint event")
+        self.draw_board()
 
     def resizeEvent(self, event):
-        print("game board")
-        print(self.width())
-        print(self.height())
+        print("resize event")
+        size = 0
+        if self.width() <= self.height():
+            size = self.width()
+            self.resize(self.width(), self.width())
+        elif self.width() > self.height():
+            size = self.height()
+            self.resize(self.height(), self.height())
+        self.BOARD_SIZE = size
+        self.CELL_SIZE = size / 8
 
 
 class MainWidget(QWidget):
-    """
-        def __init__(self, parent=None):
-        super(MainWidget, self).__init__(parent)
-    """
-
     def __init__(self):
         super().__init__()
 
@@ -97,15 +95,6 @@ class MainWidget(QWidget):
         layout.addWidget(self.player1_widget, 0, 2, 1, 1)
         layout.addWidget(self.player2_widget, 1, 2, 1, 1)
         self.setLayout(layout)
-
-        print("main widget")
-        print(self.width())
-        print(self.height())
-
-    def resizeEvent(self, event):
-        print("main widget")
-        print(self.width())
-        print(self.height())
 
 
 class Window(QMainWindow):
@@ -129,3 +118,8 @@ if __name__ == '__main__':
     window = Window()
     window.show()
     app.exec()
+
+    """
+        def __init__(self, parent=None):
+            super(MainWidget, self).__init__(parent)
+    """
