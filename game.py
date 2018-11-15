3c772236f6396dc6895e14ae8e8f193171fee9cb
GUILLON Nicolas <nicolas.guillon@epitech.eu>
2018-11-15 11:18:24 +0000

    Initialize Board game

--- game.py
+++ game.py
@@ -1,131 +1,60 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget, QAction, QFileDialog, QWidget, QLabel, \
-    QPushButton, QColorDialog, QVBoxLayout, QGridLayout, QRadioButton, QButtonGroup, QSlider, QMessageBox
-from PyQt5.QtGui import QIcon, QImage, QPainter, QPen, QColor
 import sys
 import os
-from PyQt5.QtCore import Qt, QPoint
 from enum import Enum
 
+class PlayerTurn(Enum):
+    BLACK = 0
+    WHITE = 1
 
 class CellState(Enum):
     EMPTY = 0
-    BLACK = 1
-    WHITE = 2
-    PURPLE = 3
+    BLACK_MAN = 1
+    BLACK_KING = 2
+    WHITE_MAN = 3
+    WHITE_KING = 4
 
-class Player1Widget(QWidget):
-    def __init__(self):
-        super().__init__()
+class Game:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.Cells = [[CellState.EMPTY for i in range(y)] for j in range(x)]
+        self.PlayerTurn = PlayerTurn.BLACK
+        self.Clicks = 0
 
-        label = QLabel("player 1")
+        self.InitializeBoard()
 
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        self.setLayout(layout)
-
-        self.setStyleSheet("background: red")
-
-        print("player 1")
-        print(self.width())
-        print(self.height())
-
-    def resizeEvent(self, event):
-        print("player 1")
-        print(self.width())
-        print(self.height())
-
-
-class Player2Widget(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        label = QLabel("player 2")
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        self.setLayout(layout)
-
-        self.setStyleSheet("background: blue")
-
-        print("player 2")
-        print(self.width())
-        print(self.height())
-
-    def resizeEvent(self, event):
-        print("player 2")
-        print(self.width())
-        print(self.height())
-
-
-class GameBoardWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        label = QLabel("game board")
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        self.setLayout(layout)
-
-        self.setStyleSheet("background: green")
-
-        print("game board")
-        print(self.width())
-        print(self.height())
-
-    def resizeEvent(self, event):
-        print("game board")
-        print(self.width())
-        print(self.height())
-
-
-class MainWidget(QWidget):
     """
-        def __init__(self, parent=None):
-        super(MainWidget, self).__init__(parent)
+    Initializes the board game depending on x and y board sizes.
+    For now it only fills 3 lines of pawns for each player.
     """
+    def InitializeBoard(self):
+        w, h = 8, 8
+        """
+        Setting top pawns, Black player
+        """
+        for y in range(0, 3):
+            if y % 2 == 0: start = 0
+            else: start = 1
+            for x in range(start, w, 2):
+                self.Cells[y][x] = CellState.BLACK_MAN
 
-    def __init__(self):
-        super().__init__()
+        """
+        Setting bottom pawns, White player
+        """
+        for y in range(h - 1, h - 4, -1):
+            if y % 2 == 0: start = 0
+            else: start = 1
+            for x in range(start, w, 2):
+                self.Cells[y][x] = CellState.WHITE_MAN
+        self.PrintCells()
 
-        self.game_board_widget = GameBoardWidget()
-        self.player1_widget = Player1Widget()
-        self.player2_widget = Player2Widget()
-
-        layout = QGridLayout()
-        layout.addWidget(self.game_board_widget, 0, 0, 2, 2)
-        layout.addWidget(self.player1_widget, 0, 2, 1, 1)
-        layout.addWidget(self.player2_widget, 1, 2, 1, 1)
-        self.setLayout(layout)
-
-        print("main widget")
-        print(self.width())
-        print(self.height())
-
-    def resizeEvent(self, event):
-        print("main widget")
-        print(self.width())
-        print(self.height())
-
-
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        top = 400
-        left = 400
-        width = 1280
-        height = 720
-        self.setWindowTitle("Game of Draughts")
-        self.setGeometry(left, top, width, height)
-
-        self.central_widget = MainWidget()
-        self.setCentralWidget(self.central_widget)
-        # self.central_widget.show()
+    """
+    DEBUG Method : prints the board cells
+    """
+    def PrintCells(self):
+        for x in self.Cells:
+            print(x)
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = Window()
-    window.show()
-    app.exec()
+    app = Game(8, 8)
